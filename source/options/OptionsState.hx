51713f00b27a44a27fc20a1f2c6022d09a123346
ItsIsaacIO <isaacio11509@gmail.com>
2022-07-19 22:25:04 -0400

    sdatghfdahadf

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,7 +29,7 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = ['Note Colors', 'Controls', /*'Adjust Delay and Combo',*/ 'Graphics', 'Visuals and UI', 'Gameplay'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
@@ -46,8 +46,8 @@ class OptionsState extends MusicBeatState
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
 				openSubState(new options.GameplaySettingsSubState());
-			//case 'Adjust Delay and Combo':
-				//LoadingState.loadAndSwitchState(new options.NoteOffsetState());
+			case 'Adjust Delay and Combo':
+				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 		}
 	}
 
@@ -60,7 +60,7 @@ class OptionsState extends MusicBeatState
 		#end
 
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
-		bg.color = 0xFFea71fd;
+		bg.color = 0xffff0077;
 		bg.updateHitbox();
 
 		bg.screenCenter();
